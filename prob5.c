9d034709300186a31655a883f30aca349ec2607d
Md. Daudul Islam Opu <140642063+Dawadulopu88@users.noreply.github.com>
2023-07-30 10:28:26 +0600

    Add files via upload

---
+++ prob5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+float a,b;
+//c=3.5;
+//d=7.5;
+float avg;
+scanf("%f%f",&a,&b);
+avg= (a+b)/2.0;
+printf("MEDIA = %.5f\n",avg);
+
+
+
+return 0;
+}
---
+++ problem 2.c
@@ -0,0 +1,12 @@
+ #include<stdio.h>
+ int main (){
+
+ double A,R,n;
+ n=3.14159;
+ scanf("%lf",&R);
+ A=n*(R*R);
+ printf("A=%.4lf",A);
+
+ return 0;
+
+ }
