1f5e3bac803acab9fe0a8a73fa775b11d2c271a9
Md. Daudul Islam Opu <140642063+Dawadulopu88@users.noreply.github.com>
2023-08-02 22:39:34 +0600

    Add files via upload

---
+++ sphere.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+int r;
+double p=3.14159;
+double s;
+scanf("%d",&r);
+s=(4.0/3)*p*pow(r,3);
+printf("VOLUME = %.3lf\n",s);
+
+
+
+
+return 0;
+
+}
