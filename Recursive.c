e0ac8d4ee5083e3969ba4ca3dcb7ff646cb29068
Md. Daudul Islam Opu <140642063+Dawadulopu88@users.noreply.github.com>
2023-08-20 10:53:45 +0600

    Add files via upload

---
+++ Recursive.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void printNumber(int s, int e){
+if(s>e){
+return;//Base case
+}
+printf("%d\n",s);
+printNumber(s+1,e);//recursive case
+}
+int main(){
+int s=1;
+int e=10;
+printf("Numbers from %d to %d:\n",s,e);
+printNumber(s,e);
+return 0;
+}
